87d0c9e14889b6627cd0c7c7fd476ca58e36e1c8
Nicklas Ö <nicklas@apptech.se>
2024-08-19 15:12:52 +0200

    asdasd

---
+++ app/machine/[id]/about.tsx
@@ -0,0 +1,5 @@
+import { ThemedText } from "@/components/ThemedText";
+
+export default function MachineAboutScreen() {
+  return <ThemedText>About screen</ThemedText>;
+}
---
+++ app/machine/[id]/index.tsx
@@ -0,0 +1,5 @@
+import { ThemedText } from "@/components/ThemedText";
+
+export default function MachineScreen() {
+  return <ThemedText type="title">Machine 1 page..</ThemedText>;
+}
